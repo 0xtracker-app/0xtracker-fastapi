03d0e23fe6a66087314f7a0fe35a86a1f2f40153
DESKTOP-K0HCLSG\tmont <t.monte87@gmail.com>
2021-08-22 13:42:43 -0400

    Trying to fix some errors

--- app/main.py
+++ app/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from mangum import Mangum
+from starlette.middleware.cors import CORSMiddleware
 
 from api.v1.api import router as api_router
 
 app = FastAPI(title='Serverless Lambda FastAPI')
 
 app.include_router(api_router, prefix="/api/v1")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins='*',
+    allow_credentials=False,
+    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_headers=["x-apigateway-header", "Content-Type", "X-Amz-Date"],
+)
 
 
 @app.get("/",  tags=["Endpoint Test"])
