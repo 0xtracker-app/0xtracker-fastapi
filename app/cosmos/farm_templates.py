99b52736b9c291d4874805d24525a7fd9a106b9b
DESKTOP-K0HCLSG\tmont <t.monte87@gmail.com>
2021-10-09 12:34:22 -0400

    Fixed Cosmos Staking for empty arrays

--- app/cosmos/farm_templates.py
+++ app/cosmos/farm_templates.py
@@ -23,34 +23,34 @@ async def get_delegations(wallet, session, vaults, farm_id, mongodb):
 
     for i,each in enumerate(staking):
         if 'delegation_responses' in each:
+            if len(each['delegation_responses']) > 0:
+                staked_position = {'staked' : 0, 'gambitRewards' : [], 'validators' : [], 'network' : 'cosmos'}
+                reward_token = {'pending': 0}
+                want_token = each['delegation_responses'][0]['balance']['denom']
+                staked_position.update(await TokenMetaData(address=want_token, mongodb=mongodb).lookup())
 
-            staked_position = {'staked' : 0, 'gambitRewards' : [], 'validators' : [], 'network' : 'cosmos'}
-            reward_token = {'pending': 0}
-            want_token = each['delegation_responses'][0]['balance']['denom']
-            staked_position.update(await TokenMetaData(address=want_token, mongodb=mongodb).lookup())
+                for position in each['delegation_responses']:
+                    staked_position['validators'].append(position['delegation']['validator_address'])
+                    staked_position['staked'] += float(position['balance']['amount'])
+                    staked_position['want'] = position['balance']['denom']
+                
+                if unbonded[i]['unbonding_responses']:
+                    for entry in unbonded[i]['unbonding_responses']:
+                        for pending in entry['entries']:
+                            staked_position['staked'] += float(pending['balance'])
 
-            for position in each['delegation_responses']:
-                staked_position['validators'].append(position['delegation']['validator_address'])
-                staked_position['staked'] += float(position['balance']['amount'])
-                staked_position['want'] = position['balance']['denom']
-            
-            if unbonded[i]['unbonding_responses']:
-                for entry in unbonded[i]['unbonding_responses']:
-                    for pending in entry['entries']:
-                        staked_position['staked'] += float(pending['balance'])
+                if rewards[i]['total']:
+                    for reward_position in rewards[i]['total']:
+                        reward_token['pending'] += float(reward_position['amount'])
+                        reward_token['token'] = reward_position['denom']
 
-            if rewards[i]['total']:
-                for reward_position in rewards[i]['total']:
-                    reward_token['pending'] += float(reward_position['amount'])
-                    reward_token['token'] = reward_position['denom']
+                staked_position['staked'] = helpers.from_custom(staked_position['staked'], staked_position['tkn0d'])
+                poolNest[poolKey]['userData'][want_token] = staked_position
+                poolIDs['%s_%s_want' % (poolKey, want_token)] = want_token
 
-            staked_position['staked'] = helpers.from_custom(staked_position['staked'], staked_position['tkn0d'])
-            poolNest[poolKey]['userData'][want_token] = staked_position
-            poolIDs['%s_%s_want' % (poolKey, want_token)] = want_token
-
-            reward_token['pending'] = helpers.from_custom(reward_token['pending'], staked_position['tkn0d'])
-            reward_token['symbol'] = staked_position['tkn0s']
-            poolNest[poolKey]['userData'][want_token]['gambitRewards'].append(reward_token)
+                reward_token['pending'] = helpers.from_custom(reward_token['pending'], staked_position['tkn0d'])
+                reward_token['symbol'] = staked_position['tkn0s']
+                poolNest[poolKey]['userData'][want_token]['gambitRewards'].append(reward_token)
 
     if len(poolIDs) > 0:
         return poolIDs, poolNest    
