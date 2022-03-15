30449199f143f1e6763c2c8fe8d38ee06cee3dcb
DESKTOP-K0HCLSG\tmont <t.monte87@gmail.com>
2022-03-14 21:27:57 -0400

    Added back id:False

--- app/cosmos/token_lookup.py
+++ app/cosmos/token_lookup.py
@@ -2,7 +2,7 @@ from . import queries
 
 async def get_ibc(token, network, session, cosmos_routes, cosmos_tokens):
 
-    is_found = await cosmos_tokens.find_one({'tokenID': token})
+    is_found = await cosmos_tokens.find_one({'tokenID': token}, {'_id': False})
 
     if is_found:
         return is_found
