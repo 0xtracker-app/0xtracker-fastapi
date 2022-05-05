7bc3260f8a545cbc0fd379460f7589c7ab049f9a
DESKTOP-K0HCLSG\tmont <t.monte87@gmail.com>
2022-05-05 07:42:13 -0400

    Change Osmosis RPC

--- app/cosmos/networks.py
+++ app/cosmos/networks.py
@@ -17,7 +17,7 @@ class CosmosNetwork:
         'osmosis' : {
             'wallet' : from_atom(wallet,'osmo'),
             'rpc' : 'https://rpc-osmosis.keplr.app',
-            'rest' : 'https://lcd-osmosis.blockapsis.com',
+            'rest' : 'https://rest-osmosis.ecostake.com',
 #            'rest' : 'https://lcd-osmosis.cosmostation.io',
             'chain_id' : 'osmosis-1',
             'chain_name' : 'Osmosis',
