1262ca1e53ae21db8ed5c01a1769cea72a0dfbdf
DESKTOP-K0HCLSG\tmont <t.monte87@gmail.com>
2022-08-10 12:36:37 -0400

    Fixed Juno and Crescent

--- app/cosmos/external_contracts.py
+++ app/cosmos/external_contracts.py
@@ -5,17 +5,17 @@ async def dummy_vault(session):
     return ['0xDummy']
 
 async def junoswap_vaults(session):
-    r = await make_get(session, 'https://raw.githubusercontent.com/CosmosContracts/junoswap-asset-list/main/token_list.json')
+    r = await make_get(session, 'https://raw.githubusercontent.com/CosmosContracts/junoswap-asset-list/main/pools_list.json')
     r = json.loads(r)
-    return [x['swap_address'] for x in r['tokens'] if x['swap_address'] != ""]
+    return [x['swap_address'] for x in r['pools'] if x['swap_address'] != ""]
 
 async def junoswap_locks(session):
-    staking_contracts = await make_get(session, 'https://raw.githubusercontent.com/CosmosContracts/junoswap-asset-list/main/token_list.json')
+    staking_contracts = await make_get(session, 'https://raw.githubusercontent.com/CosmosContracts/junoswap-asset-list/main/pools_list.json')
     rewarder_contracts = await make_get(session, 'https://raw.githubusercontent.com/CosmosContracts/junoswap-asset-list/main/rewards_list.json')
     s = json.loads(staking_contracts)
     r = json.loads(rewarder_contracts)
 
-    stake_by_swap = {x['swap_address'] : { 'staking_address' : x['staking_address'], 'rewards' : [] } for x in s['tokens'] if 'staking_address' in x}
+    stake_by_swap = {x['swap_address'] : { 'staking_address' : x['staking_address'], 'rewards' : [] } for x in s['pools'] if x['staking_address'] != "" }
 
     for x in r['list']:
         for rewards in x['rewards_tokens']:
